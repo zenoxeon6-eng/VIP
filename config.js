a52b2efad50c65b5d84e575582cba038998aa854
zenoxeon6-eng <zenoxeon6@gmail.com>
2026-08-17 02:08:35 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botName: "⚜️ بـوت الـكـاريـزمـا لـلـتـرويـج ⚜️",
+    officialChannel: "https://whatsapp.com/channel/0029Vb8XYYwKmCPO3sQkrJ1C",
+    admins: [
+        "66868663244@s.whatsapp.net" // رقم الأدمن بدون +
+    ],
+    port: process.env.PORT || 3000
+};
