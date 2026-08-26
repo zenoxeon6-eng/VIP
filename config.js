f46957b7c70f7a9d47b12ba5cd40034b7c5d4760
zenoxeon6-eng <zenoxeon6@gmail.com>
2026-08-26 14:10:30 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    botName: "⚜️ بـوت الـكـاريـزمـا لـلـتـرويـج ⚜️",
-    officialChannel: "https://whatsapp.com/channel/0029Vb8XYYwKmCPO3sQkrJ1C",
-    admins: [
-        "66868663244@s.whatsapp.net" // رقم الأدمن بدون +
-    ],
-    port: process.env.PORT || 3000
-};
